62fe5f70b19064e61c1f448f01ea6b913cf9e29a
abdulmazidakash <akashabdulmazid01@gmail.com>
2025-02-26 02:44:31 +0600

    module 17 almost done

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('Abdul Mazid Akash');
+console.log('Abdul Mazid Akash 🕒');
+// console.log('shakib');
+let shakib = 'shakib';
+console.log(shakib);
+console.log(typeof shakib );
+
+const infinity = 50 /0 ;
+console.log(infinity);
+
