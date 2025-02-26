66a3f173a06beb89a89520472d1176f61751e1a2
abdulmazidakash <akashabdulmazid01@gmail.com>
2025-02-26 12:12:11 +0600

    javascript first module done

--- index.js
+++ index.js
@@ -1,9 +1,53 @@
 console.log('Abdul Mazid Akash 🕒');
 // console.log('shakib');
-let shakib = 'shakib';
-console.log(shakib);
-console.log(typeof shakib );
+// let shakib = 'shakib';
+// console.log(shakib);
+// console.log(typeof shakib );
 
-const infinity = 50 /0 ;
-console.log(infinity);
+// const infinity = 50 /0 ;
+// console.log(infinity);
+
+//problem task 01
+// let totalMoney = 1000;
+// let oneKgOrange = 300;
+// let oneKgApples = 400;
+
+// const result = totalMoney - (oneKgOrange + oneKgApples);
+
+// console.log(result);
+
+//problem task 02
+
+// let matheMatics = 75.25;
+// let biology = 65;
+// let chemistry = 80;
+// let physics = 35.45;
+// let bengali = 99.50;
+
+// let sum = matheMatics + biology + chemistry + physics + bengali;
+
+// let result = sum / 5;
+// console.log(result.toFixed(2));
+
+//problem task 03
+
+// let divide = 119 % 5;
+// console.log(divide);
+
+//problem task 04
+
+// const a = isNaN('11');
+// console.log(a);
+
+// const a = isNaN(2-10);
+// console.log(a);
+
+
+
+
+
+
+
+
+
 
